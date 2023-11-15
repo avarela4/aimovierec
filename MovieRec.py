72371a0b9552cabe14d56140cd72beaef13afc97
avarela4 <avarela4@student11.cse.nd.edu>
2023-11-14 22:27:03 -0500

    progress report 3

--- MovieRec.py
+++ MovieRec.py
@@ -1,43 +1,46 @@
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-# read in data files
+# data files
 movies = pd.read_csv('movies.csv')
-ratings = pd.read_csv('ratings.csv')
-tags = pd.read_csv('tags.csv')
 
-# get user input
+# user input
 user_liked_movie = input("Enter a movie you previously liked: ")
 user_genre = input("Enter a genre you like: ")
 user_keyword = input("Enter a keyword associated with the movie you are looking for: ")
 
-# need to make the data all lowercase to compare 
+# convert inputs to lowercase
 user_liked_movie = user_liked_movie.lower()
 user_genre = user_genre.lower()
 user_keyword = user_keyword.lower()
 movies['title'] = movies['title'].str.lower()
 
-# combine the data of movies and ratings
-ratings_mean = ratings.groupby('movieId')['rating'].mean()
-movies = pd.merge(movies, ratings_mean, on='movieId')
+# TF-IDF Vectorization on movie genres and keywords
+movies['genres'] = movies['genres'].str.replace('|', ' ', regex=False)
+movies['genres_keywords'] = movies['genres'] + " " + movies['title']
+tfidf_vectorizer = TfidfVectorizer()
+tfidf_matrix = tfidf_vectorizer.fit_transform(movies['genres_keywords'])
 
-# filter 
-movies['genres'] = movies['genres'].str.replace('|', ' ')
-movies['title'] = movies['title'].str.strip()  
-movies['title'] = movies['title'].str.replace(r'\(\d{4}\)', '')  
+# cosine similarity between movies
+cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
 
-# get the movies that have something in common with what the user inputed 
-matched_movies = movies[movies['title'].str.contains(user_liked_movie)]
+# find index of the movie that matches user's liked movie
+movie_index = movies[movies['title'].str.contains(user_liked_movie)].index
 
-if matched_movies.empty:
+if len(movie_index) == 0:
     print("Movie not found. Please check the spelling or try another movie.")
     exit()
 
-# scoring based on the inputed genre and keyword
-movies['genre_match'] = movies['genres'].apply(lambda x: user_genre in x)
-movies['keyword_match'] = movies['title'].apply(lambda x: user_keyword in x)
-movies['score'] = movies['genre_match'].astype(int) + movies['keyword_match'].astype(int) + movies['rating']
+# similarity scores
+similarity_scores = list(enumerate(cosine_sim[movie_index[0]]))
 
-# get the top 5 and display
-recommended_movies = movies[movies['title'].isin(matched_movies['title']) == False].nlargest(5, 'score')['title']
+# sort movies based on similarity scores
+similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
+
+# top 5 similar movies
+top_similar_movies_indices = [score[0] for score in similarity_scores[1:6]]  # Exclude the first movie (itself)
+recommended_movies = movies.iloc[top_similar_movies_indices]['title']
+
 print("Top 5 recommended movies:")
 print(recommended_movies)
